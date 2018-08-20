b359dc543edf293910ff3ea6778fc77c586c7102
dwkim891220 <ehdn362@gmail.com>
2018-08-20 14:07:19 +0900

    test commit

--- app/src/main/java/kr/co/sugarhill/rxwithviewmodel/GlobalApplication.kt
+++ app/src/main/java/kr/co/sugarhill/rxwithviewmodel/GlobalApplication.kt
@@ -3,5 +3,7 @@ package kr.co.sugarhill.rxwithviewmodel
 import android.app.Application
 
 class GlobalApplication : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
